f541dd416ce2d2f83a788def689b658a1dc10263
Radiant0726 <yky030726@163.com>
2026-03-26 04:17:34 -0400

    update

--- eval_utils/cider/cidereval/__init__.py
+++
@@ -1,5 +0,0 @@
-__author__ = 'tylin'
-# edited by Michele Cafagna
-from cidereval.cider.cider import Cider
-from cidereval.ciderD.ciderD import CiderD
-from cidereval.scorers import cider, ciderD
--- eval_utils/cider/cidereval/tokenizer/__init__.py
+++
@@ -1,4 +0,0 @@
-__author__ = 'hfang'
-# edited by Michele Cafagna
-from cidereval.tokenizer.ptbtokenizer import PTBTokenizer
-from cidereval.tokenizer.simpletokenizer import SimpleTokenizer
